826ef94db6cae28b977b0e33514b961e01e2c783
Exargna <fox7games@mail.ru>
2023-12-29 22:34:07 +0600

    Переопределение методов

---
+++ src/Car.java
@@ -0,0 +1,44 @@
+public class Car {
+    private Engine engine;
+    private Body body;
+    private Wheels wheels;
+
+    public Car(Engine engine, Body body, Wheels wheels) {
+        this.engine = engine;
+        this.body = body;
+        this.wheels = wheels;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public void setWheels(Wheels wheels) {
+        this.wheels = wheels;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public Wheels getWheels() {
+        return wheels;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "engine=" + engine +
+                ", body=" + body +
+                ", wheels=" + wheels +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Deliver deliver = new Deliver();
-        deliver.calculateDeliverPrice(DeliverTypes.AUTO);
-        deliver.calculateDeliverPrice(DeliverTypes.WATER);
+        Body body = new Body("Седан");
+        Wheels wheels = new Wheels(4);
+        Engine engine = new Engine("BMW", 4.6);
+
+        Car car = new Car(engine, body, wheels);
+        System.out.println(car);
+
+
     }
 }
